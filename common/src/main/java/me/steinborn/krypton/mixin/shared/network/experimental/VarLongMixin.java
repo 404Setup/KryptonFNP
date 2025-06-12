83d3000121e11bec4bc1cc87baf5a0ca11379ef8
404Setup <153366651+404Setup@users.noreply.github.com>
2025-06-12 18:09:56 +0800

    port update (1.20.4 -> 1.21.1)

---
+++ common/src/main/java/me/steinborn/krypton/mixin/shared/network/experimental/VarLongMixin.java
@@ -0,0 +1,54 @@
+package me.steinborn.krypton.mixin.shared.network.experimental;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.network.VarLong;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(VarLong.class)
+public class VarLongMixin {
+    @Unique
+    private static final int DATA_BITS_PER_BYTE = 7;
+
+    @Unique
+    private static boolean krypton_Multi$hasContinuationBit(byte data) {
+        return (data & 0x80) != 0;
+    }
+
+    /**
+     * @author 404
+     * @reason test
+     */
+    @Overwrite
+    public static int getByteSize(long data) {
+        if (data == 0) return 1;
+
+        int significantBits = 64 - Long.numberOfLeadingZeros(data);
+        return (significantBits + DATA_BITS_PER_BYTE - 1) / DATA_BITS_PER_BYTE;
+    }
+
+
+    /**
+     * @author 404
+     * @reason test
+     */
+    @Overwrite
+    public static long read(ByteBuf buffer) {
+        long result = 0L;
+        int bytesRead = 0;
+
+        byte currentByte;
+        do {
+            if (bytesRead >= 10) {
+                throw new RuntimeException("VarLong too big");
+            }
+
+            currentByte = buffer.readByte();
+            result |= (long) (currentByte & 0x7F) << (bytesRead * DATA_BITS_PER_BYTE);
+            bytesRead++;
+        } while (krypton_Multi$hasContinuationBit(currentByte));
+
+        return result;
+    }
+}
