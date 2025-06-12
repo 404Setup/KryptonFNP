8dbb2a1aa5975022167605a1e954f8ef72164976
404Setup <153366651+404Setup@users.noreply.github.com>
2025-06-12 23:20:12 +0800

    a fix

--- common/src/main/java/me/steinborn/krypton/mixin/shared/network/experimental/VarLongMixin.java
+++ common/src/main/java/me/steinborn/krypton/mixin/shared/network/experimental/VarLongMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
 
-@Mixin(VarLong.class)
+@Mixin(value = VarLong.class, priority = 900)
 public class VarLongMixin {
     @Unique
     private static final int DATA_BITS_PER_BYTE = 7;
