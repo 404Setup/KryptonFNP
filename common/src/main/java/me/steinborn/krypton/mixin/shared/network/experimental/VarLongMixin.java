431ffa89816167275d1c894516b4e7e4e9b8f25c
404Setup <153366651+404Setup@users.noreply.github.com>
2025-06-15 19:50:17 +0800

    0.2.14

--- common/src/main/java/me/steinborn/krypton/mixin/shared/network/experimental/VarLongMixin.java
+++ common/src/main/java/me/steinborn/krypton/mixin/shared/network/experimental/VarLongMixin.java
@@ -1,54 +1,35 @@
 package me.steinborn.krypton.mixin.shared.network.experimental;
 
 import io.netty.buffer.ByteBuf;
+import me.steinborn.krypton.mod.shared.network.util.VarLongUtil;
 import net.minecraft.network.VarLong;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = VarLong.class, priority = 900)
 public class VarLongMixin {
-    @Unique
-    private static final int DATA_BITS_PER_BYTE = 7;
-
-    @Unique
-    private static boolean krypton_Multi$hasContinuationBit(byte data) {
-        return (data & 0x80) != 0;
-    }
-
     /**
      * @author 404
-     * @reason test
+     * @reason optimized version for VarLong
      */
     @Overwrite
     public static int getByteSize(long data) {
-        if (data == 0) return 1;
-
-        int significantBits = 64 - Long.numberOfLeadingZeros(data);
-        return (significantBits + DATA_BITS_PER_BYTE - 1) / DATA_BITS_PER_BYTE;
+        return VarLongUtil.getVarLongLength(data);
     }
-
 
     /**
      * @author 404
-     * @reason test
+     * @reason optimized version for VarLong (test)
      */
     @Overwrite
-    public static long read(ByteBuf buffer) {
-        long result = 0L;
-        int bytesRead = 0;
-
-        byte currentByte;
-        do {
-            if (bytesRead >= 10) {
-                throw new RuntimeException("VarLong too big");
-            }
-
-            currentByte = buffer.readByte();
-            result |= (long) (currentByte & 0x7F) << (bytesRead * DATA_BITS_PER_BYTE);
-            bytesRead++;
-        } while (krypton_Multi$hasContinuationBit(currentByte));
-
-        return result;
+    public static ByteBuf write(ByteBuf buffer, long value) {
+        if ((value & VarLongUtil.MASK_7_BITS) == 0L) {
+            buffer.writeByte((int) value);
+        } else if ((value & VarLongUtil.MASK_14_BITS) == 0L) {
+            VarLongUtil.writeTwoBytes(buffer, value);
+        } else {
+            VarLongUtil.writeVarLongFull(buffer, value);
+        }
+        return buffer;
     }
 }
