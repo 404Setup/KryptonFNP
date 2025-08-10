f14d53a37c11f154990b0ca98f16915ce1697f3e
404Setup <153366651+404Setup@users.noreply.github.com>
2025-08-10 21:18:38 +0800

    fix: Mixin not disabled correctly
    The env detection conflicts with the config detection. The env detection is now removed.

--- common/src/main/java/one/pkg/mod/krypton_fnp/shared/ModMixinBootstrap.java
+++ common/src/main/java/one/pkg/mod/krypton_fnp/shared/ModMixinBootstrap.java
@@ -13,11 +13,11 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModMixinBootstrap implements IMixinConfigPlugin {
-    static {
+    private final Logger logger = LoggerFactory.getLogger("ModMixinBootstrap");
+
+    public ModMixinBootstrap() {
         ModConfig.config.addConfigurations(); // Initialize it
     }
-
-    private final Logger logger = LoggerFactory.getLogger("ModMixinBootstrap");
 
     @Override
     public void onLoad(String mixinPackage) {
@@ -34,7 +34,7 @@ public class ModMixinBootstrap implements IMixinConfigPlugin {
         CONFIG config = CONFIG.find(mixinClassName);
         if (config != null) {
             var b = config.isEnabled();
-            logger.info("Mixin {} {} {}", mixinClassName, b ? "enabled" : "disabled", config.ENV);
+            logger.info("Mixin {} {}", mixinClassName, b ? "enabled" : "disabled");
             return b;
         }
         return true;
@@ -61,19 +61,17 @@ public class ModMixinBootstrap implements IMixinConfigPlugin {
     }
 
     enum CONFIG {
-        Login_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.ServerLoginPacketListenerImplMixin", "krypton.loginVT", ModConfig.Mixin::isLoginVT),
-        TextFilter_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.ServerTextFilterMixin", "krypton.textFilterVT", ModConfig.Mixin::isTextFilterVT),
-        Util_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.UtilMixin", "krypton.utilVT", ModConfig.Mixin::isUtilVT),
-        BestVarLong("one.pkg.mod.krypton_fnp.mixin.network.experimental.VarLongMixin", "krypton.bestVarLong", ModConfig.Mixin::isBestVarLong),
+        Login_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.ServerLoginPacketListenerImplMixin", ModConfig.Mixin::isLoginVT),
+        TextFilter_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.ServerTextFilterMixin", ModConfig.Mixin::isTextFilterVT),
+        Util_VT("one.pkg.mod.krypton_fnp.mixin.network.experimental.UtilMixin", ModConfig.Mixin::isUtilVT),
+        BestVarLong("one.pkg.mod.krypton_fnp.mixin.network.experimental.VarLongMixin", ModConfig.Mixin::isBestVarLong),
         ;
 
         public final String CLASS;
-        public final String ENV;
         public final Supplier<Boolean> configTarget;
 
-        CONFIG(String clazz, String env, @NotNull Supplier<Boolean> configTarget) {
+        CONFIG(String clazz, @NotNull Supplier<Boolean> configTarget) {
             this.CLASS = clazz;
-            this.ENV = env;
             this.configTarget = configTarget;
         }
 
@@ -88,7 +86,7 @@ public class ModMixinBootstrap implements IMixinConfigPlugin {
         }
 
         public boolean isEnabled() {
-            return this.configTarget.get() || Boolean.parseBoolean(System.getProperty(ENV, "true"));
+            return this.configTarget.get();
         }
     }
 }
