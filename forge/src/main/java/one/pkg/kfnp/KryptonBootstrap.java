091dd5ef210e2161ffd62936fcd8b95600817806
404Setup <153366651+404Setup@users.noreply.github.com>
2026-01-16 00:01:27 +0800

    closed: #16

--- forge/src/main/java/one/pkg/kfnp/KryptonBootstrap.java
+++ forge/src/main/java/one/pkg/kfnp/KryptonBootstrap.java
@@ -1,11 +1,27 @@
 package one.pkg.kfnp;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import one.pkg.kfnp.shared.ModSharedBootstrap;
+import one.pkg.kfnp.shared.gui.KFNPConfigGUI;
+import one.pkg.loader.Loader;
 
 @Mod("krypton_fnp")
 public class KryptonBootstrap {
     public KryptonBootstrap() {
         ModSharedBootstrap.run();
+
+        if (Loader.INSTANCE.isClient()) init();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static void init() {
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((mc, parent) -> new KFNPConfigGUI(parent))
+        );
     }
 }
