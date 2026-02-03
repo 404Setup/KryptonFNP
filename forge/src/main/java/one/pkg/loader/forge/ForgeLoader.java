e119ac3357451fc89d2ea001238cc71a0b2f191d
404Setup <153366651+404Setup@users.noreply.github.com>
2026-02-03 17:36:06 +0800

    Compatible with e4mc 6. (#18)

--- forge/src/main/java/one/pkg/loader/forge/ForgeLoader.java
+++ forge/src/main/java/one/pkg/loader/forge/ForgeLoader.java
@@ -2,7 +2,10 @@ package one.pkg.loader.forge;
 
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
+import one.pkg.loader.FMLMod;
 import one.pkg.loader.LoaderImpl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
 
@@ -16,5 +19,16 @@ public class ForgeLoader implements LoaderImpl {
     @Override
     public boolean isClient() {
         return FMLLoader.getDist().isClient();
+    }
+
+    @Override
+    public boolean loaded(@NotNull String modid) {
+        return FMLLoader.getLoadingModList().getModFileById(modid) != null;
+    }
+
+    @Override
+    public @Nullable FMLMod mod(@NotNull String modid) {
+        var m = FMLLoader.getLoadingModList().getModFileById(modid);
+        return m == null ? null : new ForgeMod(m);
     }
 }
