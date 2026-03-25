aea4d07c1d6959ee010df84f9a8594419fbaf991
404Setup <153366651+404Setup@users.noreply.github.com>
2026-03-25 09:48:12 +0800

    some

--- common/src/main/java/one/pkg/loader/FMLTest.java
+++ common/src/main/java/one/pkg/loader/FMLTest.java
@@ -1,7 +1,11 @@
 package one.pkg.loader;
 
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.Internal
 public interface FMLTest {
     // This is a deliberate check.
+    @ApiStatus.Internal
     static void test() {
         try {
             Class.forName("org.bukkit.advancement.Advancement");
