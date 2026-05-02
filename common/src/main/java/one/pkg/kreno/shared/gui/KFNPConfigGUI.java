16a4e792f5955446af3fe025a2e2187e71f67eab
404Setup <153366651+404Setup@users.noreply.github.com>
2026-05-02 14:50:34 +0800

    .

---
+++ common/src/main/java/one/pkg/kreno/shared/gui/KFNPConfigGUI.java
@@ -0,0 +1,21 @@
+package one.pkg.kreno.shared.gui;
+
+import net.minecraft.Util;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import one.pkg.loader.Loader;
+
+public class KFNPConfigGUI extends Screen {
+    private final Screen parent;
+
+    public KFNPConfigGUI(Screen parent) {
+        super(Component.empty());
+        this.parent = parent;
+    }
+
+    @Override
+    protected void init() {
+        Util.getPlatform().openUri(Loader.INSTANCE.getConfigPath().resolve("krypton_fnp.yaml").toUri());
+        minecraft.setScreen(parent);
+    }
+}
---
+++ forge/src/main/java/one/pkg/kreno/KRenoModStart.java
@@ -0,0 +1,25 @@
+package one.pkg.kreno;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import one.pkg.kreno.shared.ModSharedBootstrap;
+import one.pkg.kreno.shared.gui.KFNPConfigGUI;
+import one.pkg.loader.FMLTest;
+import one.pkg.loader.Loader;
+
+@Mod("kreno")
+public class KRenoModStart {
+    public KRenoModStart() {
+        FMLTest.test();
+        ModSharedBootstrap.run();
+
+        if (Loader.INSTANCE.isClient()) init();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private static void init() {
+        MinecraftForge.registerConfigScreen(KFNPConfigGUI::new);
+    }
+}
